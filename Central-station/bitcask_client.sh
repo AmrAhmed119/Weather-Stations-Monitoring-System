54bc223d1eda271df13fc18ba900b1d4df902bef
abdelmotlb10 <abdelmotlb10@gmail.com>
2025-05-24 22:26:53 +0300

    client-shell.sh for 3 interface functions

--- Central-station/bitcask_client.sh
+++ Central-station/bitcask_client.sh
@@ -1,23 +1,51 @@
+# #!/bin/bash
+
+# # Compile the project
+# mvn clean compile
+
+# # Run the CLI tool
+# if [[ $1 == "--view-all" ]]; then
+#     mvn exec:java -Dexec.mainClass="com.centralstation.BitcaskClient.Client" -Dexec.args="--view-all" -e
+
+# elif [[ $1 == --view* ]]; then
+#     key_arg=$1
+#     mvn exec:java -Dexec.mainClass="com.centralstation.BitcaskClient.Client" -Dexec.args="$key_arg"
+
+# elif [[ $1 == --perf* ]]; then
+#     perf_arg=$1
+#     mvn exec:java -Dexec.mainClass="com.centralstation.BitcaskClient.Client" -Dexec.args="$perf_arg"
+
+# else
+#     echo "Usage:"
+#     echo "  ./bitcask_client.sh --view-all"
+#     echo "  ./bitcask_client.sh --view=KEY"
+#     echo "  ./bitcask_client.sh --perf=100"
+# fi
+
+
 #!/bin/bash
 
-# Compile the project
-mvn clean compile
+BASE_URL="http://localhost:8080"
 
-# Run the CLI tool
-if [[ $1 == "--view-all" ]]; then
-    mvn exec:java -Dexec.mainClass="com.centralstation.BitcaskClient.Client" -Dexec.args="--view-all" -e
+case "$1" in
+  --get)
+    if [[ -z "$2" ]]; then
+      echo "Usage: ./bitcask_client.sh --get KEY"
+      exit 1
+    fi
+    curl -s "$BASE_URL/get?key=$2"
+    echo
+    ;;
 
-elif [[ $1 == --view* ]]; then
-    key_arg=$1
-    mvn exec:java -Dexec.mainClass="com.centralstation.BitcaskClient.Client" -Dexec.args="$key_arg"
+  --view-all)
+    curl -s "$BASE_URL/all"
+    echo
+    ;;
 
-elif [[ $1 == --perf* ]]; then
-    perf_arg=$1
-    mvn exec:java -Dexec.mainClass="com.centralstation.BitcaskClient.Client" -Dexec.args="$perf_arg"
-
-else
+  *)
     echo "Usage:"
-    echo "  ./bitcask_client.sh --view-all"
-    echo "  ./bitcask_client.sh --view=KEY"
-    echo "  ./bitcask_client.sh --perf=100"
-fi
+    echo "  ./bitcask_client.sh --put KEY VALUE"
+    echo "  ./bitcask_client.sh --get KEY"
+    echo "  ./bitcask_client.sh --all"
+    ;;
+esac
